d776b542562163fc17e5a12058a7d721f53b5a67
farzamshahid <farzamshahid6@gmail.com>
2024-03-26 13:24:01 -0700

    all files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import RandomColor from './Components/RandomColor'
+
+function App() {
+
+  return (
+
+    <div>
+      <RandomColor />
+    </div>
+  )
+}
+
+export default App
